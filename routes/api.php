8713ecefb21e38e03fef034608effcc017fc89eb
SheikhFaizan141 <faizanfarooq141@gmail.com>
2024-02-17 22:22:32 +0530

    sesssions routes added need to check for edge cases and rund some testing

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\MealController;
 use App\Models\Meal;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Validation\Rule;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,27 +29,53 @@ Route::get('meal/{meal}', [MealController::class, 'show'])
     ->missing(fn () => response()->json([], 404));
 
 
+// Login
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
 
 
+        return response()->json(['message' => "welcome back"]);
+    }
+
+    return response()->json(['email' => 'The provided credentials do not match our records.'], 401);
+});
 
 
+Route::post('/signup', function (Request $request) {
+    $attributes = $request->validate([
+        'name' => ['required'],
+        // 'email' => ['required', 'email', 'unique:users,email'],
+        'email' => ['required', 'email', Rule::unique('users', 'email')],
+        'password' => ['required', 'min:3']
+    ]);
 
 
+    $user = User::create($attributes);
 
+    Auth::login($user);
 
+    return response()->json(['message' => 'user created']);
+});
 
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    Auth::guard('web')->logout();
+    // Auth::logout();
 
-
-
-
+    $request->session()->invalidate();
+ 
+    $request->session()->regenerateToken();
     
+    return response()->json(['message' => 'user is loged out']);
+});
 
-// Route::get('/search', function (Request $request) {
-//     $meal = Meal::latest()->filter(request(['q']))->paginate(12);
-//     return response()->json($meal);
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+// Route::middleware('auth:sanctum')->get('/admin', function);
